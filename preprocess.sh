18f9606c8a6e62524056642f8cb50e0248b3fbbe
siddsabata <siddharth@sabata.net>
2025-04-09 12:50:55 -0400

    syntax error

--- preprocess.sh
+++ preprocess.sh
@@ -6,13 +6,9 @@
 #SBATCH --cpus-per-task=5
 #SBATCH --mem=16G
 
-# get inputs
-in_csv=$1
-output_dir=$2
-
 # activate preprocess environment
 source ~/miniconda3/bin/activate
 conda activate preprocess_env
 
 # run preprocess.py
-python3 0-preprocess/process_tracerX.py -i $in_csv -o $output_dir 
+python3 0-preprocess/process_tracerX.py -i $1 -o $2 
