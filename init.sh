ce14a3e21462f528ffd09bccbbed294f6242d4d6
siddsabata <siddharth@sabata.net>
2025-04-09 12:36:43 -0400

    fixed phylowgs install

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@
 mkdir -p logs
 
 # install phylowgs 
-cd 2-phylowgs/phylowgs
+cd 2-phylowgs
 git clone https://github.com/morrislab/phylowgs.git
+cd phylowgs
 echo "repo cloned; compiling phylowgs"
 g++ -o mh.o -O3 mh.cpp util.cpp `gsl-config --cflags --libs`
 cd ../../
